040b1ae46f1be7685cf8f26d8772db946021b4ed
Blaine Anderson <andersonblaine90@gmail.com>
2016-08-13 18:31:12 -0400

    Fixed the FizzBuzz Checker. Woohoo

--- js/fizzbuzz-checker.js
+++ js/fizzbuzz-checker.js
@@ -2,19 +2,28 @@ var main = function() {
 	$("form").submit(function() {
 		var value = $("input").val();
 		var resultArea = "#resultArea";
+		var clear = $("input").val("");
 
 		if (value % 3 === 0 && value % 5 === 0) {
-			var result = "<p><b>FizzBuzz!</b></p><p>That was awesome! Try another number if you like.</p>";
-			$(result).appendTo(resultArea);
+			var result = "<p><b>" + value + " = FizzBuzz!</b></p><p>That was awesome! Try another number if you like.</p>";
+			$(result).prependTo(resultArea);
+			return false;
+			clear;
 		} else if (value % 5 === 0) {
-	        var result = "<p><b>Buzz!</b></p><p>Woah! So cool! Feel free to try it again.</p>";
-			$(result).appendTo(resultArea);
+	        var result = "<p><b>" + value + " = Buzz!</b></p><p>Woah! So cool! Feel free to try it again.</p>";
+			$(result).prependTo(resultArea);
+			return false;
+			clear;
 	    } else if (value % 3 === 0) {
-	        var result = "<p><b>Fizz!</b></p><p>Far out! You know you wanna try another number...</p>";
-			$(result).appendTo(resultArea);
+	        var result = "<p><b>" + value + " = Fizz!</b></p><p>Far out! You know you wanna try another number...</p>";
+			$(result).prependTo(resultArea);
+			return false;
+			clear;
 	    } else {
-	        var result = "<p>" + value + "</p><p>Well...that was boring...try again!</p>";
-			$(result).appendTo(resultArea);
+	        var result = "<p><b>" + value + "</b></p><p>Well...that was boring...try again!</p>";
+			$(result).prependTo(resultArea);
+			return false;
+			clear;
 	    }
 	});
 };
