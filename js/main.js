97422e3b9775a70dddbcfcaca29aaac83cc3422c
Blaine Anderson <andersonblaine90@gmail.com>
2016-08-16 21:55:28 -0400

    Did more CSS cleaning. Started on mobile navigation.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 //Add scripts for mobile header design
+
+
+var main = function() {
+	$(document).on("click", "#menu-expand", function() {
+		$("header > nav > a").css("display", "block");
+	});
+}
+
+$(document).ready(main);
