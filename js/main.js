a2b082c6138a93a7eaad1e08928a6ef9b5975f7b
Blaine Anderson <andersonblaine90@gmail.com>
2016-08-22 11:31:21 -0400

    Made some stylistic changes. Mobile nav isn't perfect. But it will do for now.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 //Add scripts for mobile header design
 
-
 var main = function() {
 	$(document).on("click", "#menu-expand", function() {
-		$("#nav").toggle();
+		$("#nav").css("display", "block");
+		$("#menu-expand").css("display", "none");
+		$("#menu-collapse").css("display", "block");
+	});
+
+	$(document).on("click", "#menu-collapse", function() {
+		$("#nav").css("display", "none");
+		$("#menu-collapse").css("display", "none");
+		$("#menu-expand").css("display", "inline-block");
 	});
 }
 
