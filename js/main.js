0802719b3af265cbfd3adc23e9ff4d56994809e4
Blaine Anderson <andersonblaine90@gmail.com>
2016-12-07 19:29:44 -0500

    Redesigned the navigation and hopefully made it work on mobile devices.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
 //Add scripts for mobile header design
 
 var main = function() {
-	$(document).on("click", "#menu-expand", function() {
-		$("#nav").css("display", "block");
-		$("#menu-expand").css("display", "none");
-		$("#menu-collapse").css("display", "block");
+	$(document).on("click", "#menu-link", function() {
+		$("#nav").toggle();
 	});
 
-	$(document).on("click", "#menu-collapse", function() {
-		$("#nav").css("display", "none");
-		$("#menu-collapse").css("display", "none");
-		$("#menu-expand").css("display", "inline-block");
-	});
 }
 
 $(document).ready(main);
