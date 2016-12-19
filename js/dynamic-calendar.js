6d28c0057c3e11e2796c3e9e8ea2db0b39241f63
Blaine Anderson <andersonblaine90@gmail.com>
2016-12-19 07:48:10 -0500

    Wrote down some thoughts about how to best create the calendar I want.

---
+++ js/dynamic-calendar.js
@@ -0,0 +1,7 @@
+/*
+Things Needed:
+- Set a start date, and have the computer generate 2 months of a calendar
+- Need create the calendar structure
+- Need a way to target dates on the calendar after it has been created
+
+*/
