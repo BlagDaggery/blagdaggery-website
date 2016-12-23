a85305d5bb6380c8a41de250ac79871d8b2da108
Blaine Anderson <andersonblaine90@gmail.com>
2016-12-23 13:11:28 -0500

    Added form for calendar controls. Started mapping out JavaScript for calendar functionality.

--- js/dynamic-calendar.js
+++ js/dynamic-calendar.js
@@ -1,7 +1,39 @@
 /*
-Things Needed:
-- Set a start date, and have the computer generate 2 months of a calendar
-- Need create the calendar structure
-- Need a way to target dates on the calendar after it has been created
+var distanceLearningColor = $accent-1;
+var weekendResidenciesColor = $accent-2;
+var globalResidencyColor = $accent-3;
+var distanceLearningSessions = 7;
+
+functions for highlighting different days
+distanceLearning {
+	highlight distance learning days based on cville vs dc
+	count the number of Tuesdays and Thursdays and highlight every:
+		- X & Y Tuesday
+		- X & Y Thursday
+}
+
+weekendResidencies {
+	highlight weekend residencies based on Cville vs DC
+	count the number of weekends, and highlight the X weekend
+}
+
+highlightGlobalResidency {
+	highlight the global residency based on the selection of:
+		- EMBA vs GEMBA
+		- Residency vs No Resdiency
+
+}
+
+main function
+
+when the form is submitted, call the three highlight functions
+
+*/
+
+
+/*
+Ways to Improve
+
+- Have the browser dynamically create the calendar
 
 */
