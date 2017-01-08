59fc45039933a173d0666bc21d4dcfe49e85b878
Blaine Anderson <andersonblaine90@gmail.com>
2017-01-07 19:56:01 -0500

    Followed a tutorial on how to make a calendar with JavaScript. Does not work yet. Come back to this.

--- js/dynamic-calendar.js
+++ js/dynamic-calendar.js
@@ -1,11 +1,80 @@
-var distanceLearningColor = $accent-1;
-var weekendResidenciesColor = $accent-2;
-var globalResidencyColor = $accent-3;
-var distanceLearningSessions = 7;
-var location = $('#location').val();
-var format = $('#format').val();
-var residency = $('#residency').val();
-var distance = $('#distance');
+//var distanceLearningColor = $accent-1;
+//var weekendResidenciesColor = $accent-2;
+//var globalResidencyColor = $accent-3;
+//var distanceLearningSessions = 7;
+//var location = $('#location').val();
+//var format = $('#format').val();
+//var residency = $('#residency').val();
+//var distance = $('#distance');
+
+var daysLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+var monthsLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var currentDate = new Date();
+
+function Calendar(month, year) {
+	this.month = (isNaN(month) || month == null) ? currentDate.getMonth() : month;
+	this.year = (isNaN(year) || year ==null) ? currentDate.getFullYear() : year;
+	this.html = '';
+}
+
+Calendar.prototype.generateHTML = function() {
+	// get first day of month
+	var firstDay = new Date(this.year, this.month, 1);
+	var startingDay = firstDay.getDay();
+
+	// find number of days in month
+	var monthLength = daysInMonth[this.month];
+
+	// compensate for leap year
+	if(this.month === 1) {
+		if((this.year % 4 == 0 && this.year % 100 !=0) || this.year % 400 ==0) {
+			monthLength = 29;
+		}
+	}
+
+	// do the header
+	var monthName = monthsLabels[this.month];
+	var html = '<table class="calendar-table">';
+	html += '<tr><th colspan="7">';
+	html += monthName + "&nbsp;" + this.year;
+	html += '</th></tr>';
+	html += '<tr class="calendar-header">';
+	for (var i = 0; i <= 6; i++){
+		html += '<td class="calendar-header-day">';
+		html += daysLabels[i];
+		html += '</td>';
+	}
+	html += '</tr><tr>';
+
+	// start filling in the days
+	var day = 1;
+	for (var i = 0; i < 9; j++) {
+		for (var j = 0; j <= 6; j++) {
+			html += '<td class="calendar-day">';
+			if (day <= monthLength && (i > 0 || j >= startingDay)) {
+				html += day;
+				day++;
+			}
+			html += '</td>';
+		}
+		// stop making rows if we've run out of days
+		if (day > monthLength) {
+			break;
+		} else {
+			html += '</tr><tr>';
+		}
+	}
+
+	html += '</tr></table>';
+
+	this.html = html;
+}
+
+Calendar.prototype.getHTML = function() {
+	return this.html;
+}
+
 
 /*
 functions for highlighting different days
